47cc1ba47a660fbd7b800b93143fd6e455a17f6a
周皮皮爱思考 <313415528@qq.com>
2026-06-20 13:49:16 +0800

    fix: 添加include=['tasks']到Celery配置，修复任务未注册问题

--- celery_app.py
+++ celery_app.py
@@ -4,7 +4,8 @@ from config import Config
 celery_app = Celery(
     'grade7_new',
     broker=Config.CELERY_BROKER_URL,
-    backend=Config.CELERY_RESULT_BACKEND
+    backend=Config.CELERY_RESULT_BACKEND,
+    include=['tasks']  # 关键：自动导入tasks.py，注册异步任务
 )
 
 celery_app.conf.update(
